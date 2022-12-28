6c529a1ebb9d1029c1b125cb68a4640a36eeeda1
estiBarel <esti.barel@wibbitz.com>
2022-12-28 16:30:00 +0200

    final EX4- outter.html -about me

--- Aboutme/main.js
+++ Aboutme/main.js
@@ -1,12 +1,12 @@
-// function handleClick(id) {
-//     console.log('Element Clicked');
-//     document.getElementById(id).style.color="red";
+function handleClick(id) {
+    console.log('Element Clicked');
+    document.getElementById(id).style.color="red";
     
-// }
+}
 
-// function delayHandleClick (id) {
-//     setTimeout {function handleClick(id) , 3000};
-// }
+function delayHandleClick (id) {
+    setTimeout {function handleClick(id) , 3000};
+}
 
 
 
@@ -23,19 +23,3 @@ makeList += `</ul>`;
 console.log(makeList);
 document.getElementById("all-Food").outerHTML = makeList;
 
-// const movies = ["Django", "Seven", "Batman"];
-// let makeList = `<ul>`;
-// for (let index = 0; index < movies.length; index++) {
-//   const element = movies[index];
-//   // element = "Django" (index = 0)
-//   // element = "Seven" (index = 1)
-//   makeList += `<li>${element}</li>`;
-//   //makeList = <ul><li>Django</li> (index = 0)
-//   //makeList = <ul><li>Django</li><li>Seven</li> (index = 1)
-//   // ...
-// }
-// makeList += `</ul>`;
-// console.log(makeList);
-// //output: "<ul><li>Django</li><li>Seven</li><li>Batman</li></ul>"
-// document.getElementById("my-id").outerHTML = makeList;
-// // document.getElementById("my-id").outerHTML = "<p>Hello</p><p>Hello2</p>";
